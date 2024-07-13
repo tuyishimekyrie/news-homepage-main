058ba678bf4673e63662624167d510476c84485e
Tuyishime Kyrie <tuyishimekyrie@gmail.com>
2024-07-13 12:13:10 +0200

    add javascript logics

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
+// Javascript Logics
+
 const menuOpen = document.querySelector('.menu_toggle')
 const menuUl = document.querySelector('.menu ul')
-console.log(menuUl);
 
 menuOpen.addEventListener('click', () =>
-// console.log('d')
 menuUl.classList.toggle('open')
 )
